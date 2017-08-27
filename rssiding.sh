940a4ab3bdf6a30610a23e0522e128b38d8a8fc5
Radek Daniluk <radek0123@gmail.com>
2017-08-27 09:19:55 +0200

    Added local script which plays sound using play command from sox package

---
+++ rssiding.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+user='root'
+host='192.168.1.1'
+remote="$user@$host"
+dir=/tmp/
+file=rssi.sh
+
+set -e  # exit immediately if a command exits with a non-zero status
+set -x	# print commands
+
+scp $file $remote:$dir
+ssh $remote 'chmod u+x '$dir$file
+
+while : ; do
+ssh $remote "$dir./$file /dev/ttyACM1 0" | awk -F"[= ]" '/^([0-9]{2}:){2}[0-9]{2} RSSI/ { system("play -n synth 0.5 sin %$3-10 2>/dev/null") }'
+done;
+
+
